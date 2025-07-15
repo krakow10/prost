5d05d4d2371e7dc5589dd545f86a215b0497e7a0
Rhys Lloyd <krakow20@gmail.com>
2025-07-15 01:10:41 -0700

    relax redundant cfg - std is force-enabled by serde feature

--- prost-types/src/lib.rs
+++ prost-types/src/lib.rs
@@ -47,7 +47,7 @@ pub mod compiler;
 mod datetime;
 #[rustfmt::skip]
 mod protobuf;
-#[cfg(all(feature = "serde", feature = "std"))]
+#[cfg(feature = "serde")]
 mod serde;
 
 use core::convert::TryFrom;
