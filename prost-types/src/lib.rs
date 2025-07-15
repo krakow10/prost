6a0465093e8db0d7b15961d41d402dbbf02e6735
Rhys Lloyd <krakow20@gmail.com>
2025-07-14 23:20:32 -0700

    add serde feature flag doc to lib.rs

--- prost-types/src/lib.rs
+++ prost-types/src/lib.rs
@@ -36,6 +36,7 @@
 //! ## Feature Flags
 //! - `std`: Enable integration with standard library. Disable this feature for `no_std` support. This feature is enabled by default.
 //! - `arbitrary`: Enable integration with crate `arbitrary`. All types on this crate will implement `trait Arbitrary`.
+//! - `serde`: Enable integration with crate `serde`. The ListValue, Struct, and Value types will implement `serde::Serialize` and `serde::Deserialize`.
 //!
 //! [1]: https://developers.google.com/protocol-buffers/docs/reference/google.protobuf
 
